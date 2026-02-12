428dce13172f777ae66ddf53e8a93ba5a2876adf
Arjun Dashrath <arjundashrath@Arjuns-MacBook-Air.local>
2026-02-12 14:36:49 +0530

    Fix subdirectory paths for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://thewristjournal.com",
+	site: "https://arjundashrath.github.io",
+	base: "/mywatchblog",
 	integrations: [mdx(), sitemap()],
 });

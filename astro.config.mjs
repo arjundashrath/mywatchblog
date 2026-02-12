9098fa49cf1b2eff801ddc087f39d6adb9432c65
Arjun Dashrath <arjundashrath@Arjuns-MacBook-Air.local>
2026-02-12 16:03:33 +0530

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://arjundashrath.github.io",
-	base: "/mywatchblog",
+	site: "https://thewristjournal.com",
 	integrations: [mdx(), sitemap()],
 });
